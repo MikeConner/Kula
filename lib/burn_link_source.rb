c307612d6a7214e3ae3a9511e4817512c6e2b0bf
Arash <arash@tapdatapp.co>
2015-10-24 12:28:08 -0400

    burn_links by batch

--- lib/burn_link_source.rb
+++ lib/burn_link_source.rb
@@ -4,22 +4,32 @@ require 'uri'
 
 
 class BurnLinkSource
-  # connect_url should look like;
-  # mysql://user:password@localhost/dbname
-
-
+  DEFAULT_BATCH_SIZE = 10000
   def initialize(connect_hash)
     @mysql = Mysql2::Client.new(connect_hash)
+    @batch_size = ENV['BATCH_SIZE'].to_i || DEFAULT_BATCH_SIZE
+    @last_burn_id = ENV['LAST_BURN_ID']
   end
 
 #connect_hash(connect_url)
   def each
-    results = @mysql.query('select * from burn_links  ', as: :hash, symbolize_keys: true)
-    results.each do |row|
+    query = "select * from kula.burn_links "
 
+    unless @last_burn_id.nil?
+      query += " WHERE burn_link_id > '#{@last_burn_id}'"
+    end
+    query += " ORDER BY burn_link_id ASC LIMIT #{@batch_size}"
+    puts query
+    results = @mysql.query(query, as: :hash, symbolize_keys: true)
+    results.each do |row|
       yield(row)
     end
   end
+
+
+
+
+
 
 
 end
