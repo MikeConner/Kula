ddbf6f35739a59f6d754cb145e38b06201d3df5b
Jeff <endymionjkb@gmail.com>
2015-11-08 22:14:55 -0500

    Implement settings update

--- app/controllers/global_settings_controller.rb
+++ app/controllers/global_settings_controller.rb
@@ -9,10 +9,22 @@ class GlobalSettingsController < ApplicationController
   end
   
   def update
+    @settings = GlobalSetting.first
+
+    period = Date.parse("#{global_settings_params['current_period(1i)']}-#{global_settings_params['current_period(2i)'].rjust(2, '0')}-01")
     
+    if @settings.update_attribute(:current_period, period)
+      redirect_to site_admin_path, :notice => 'Settings successfully updated'
+    else
+      redirect_to edit_global_setting_path(@settings), :alert => 'Setting update failed!'
+    end    
   end
       
 private
+  def global_settings_params
+    params.require(:global_setting).permit('current_period(1i)', 'current_period(2i)')
+  end
+
   def ensure_admin
     unless current_user.any_admin?
       redirect_to root_path, :alert => I8n.t('admins_only')
